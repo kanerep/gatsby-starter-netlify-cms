4faf9ac7612b38e7868e28d7b8b17249b937f07d
Kane Elliott-Peace <kane.e@mmtdigital.co.uk>
2020-05-18 20:24:13 +0100

    fix build error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/slc-logo.jpeg'
+// import logo from '../img/slc-logo.jpeg'
 
 const Navbar = class extends React.Component {
 	constructor(props) {
